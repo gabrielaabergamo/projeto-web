b90549b131ef92fc3d59c88067eae16ffed1700c
LucasEPO <lucas.epoliveira@gmail.com>
2023-12-11 05:29:52 -0300

    Deixando variavel quantidade de pontos em uma rota

--- target/projeto-web/controllers/cadastro.js
+++ target/projeto-web/controllers/cadastro.js
@@ -17,88 +17,15 @@ $(document).ready(function () {
             });
 
             updateSelect(linhas);
+            updatePontos(linhas[0].pontos.join(', '));
+            updateErros();
+                 
         }
     });
-
-    let erroPonto1 = document.getElementById('erro-ponto1'),
-        erroPonto2 = document.getElementById('erro-ponto2'),
-        erroPonto3 = document.getElementById('erro-ponto3'),
-        erroPonto4 = document.getElementById('erro-ponto4'),
-        erroPonto5 = document.getElementById('erro-ponto5'),
-        erroPonto6 = document.getElementById('erro-ponto6')
-
-    let letter_only = /[^a-z]/;
-    let textLength = 5;
-
-    const elementsWrapper = [
-        {
-            input: 'ponto1',
-            error: erroPonto1
-        },
-        {
-            input: 'ponto2',
-            error: erroPonto2
-        },
-        {
-            input: 'ponto3',
-            error: erroPonto3
-        },
-        {
-            input: 'ponto4',
-            error: erroPonto4
-        },
-        {
-            input: 'ponto5',
-            error: erroPonto5
-        },
-        {
-            input: 'ponto6',
-            error: erroPonto6
-        }
-    ]
-
-    elementsWrapper.forEach((el) => {
-        $('#' + el.input).keyup(function() {
-        let element = $(this);
-        let replaced;
-
-            if(letter_only.test(element.val())){
-                letterOnlyErrMsg(el.error)
-            } else if(element.val().length < textLength ){
-                textLengthErrMsg(el.error)
-            }
-
-        replaced = element.val().replace(letter_only, '')
-        element.val(replaced)
-        })
-        $('#' + el.input).focusout(function() {
-            removeErrMsg(el.error)
-        })
-    });
-
-
-    const letterOnlyErrMsg = (erroID) => {
-        $(erroID).fadeIn('slow', function () {
-            $(this).html('O input aceita apenas letras.')
-        })
-    }
-
-    const textLengthErrMsg = (erroID) => {
-        $(erroID).fadeIn('slow', function () {
-            $(this).html('O input requer pelo menos 5 letras.')
-        })
-    }
-
-    const removeErrMsg = (erroID) => {
-        $(erroID).fadeOut(1000, function () {
-            $(this).html('')
-        })
-    }
+    
 
     $('#form').submit(function (event) {
         event.preventDefault();
-
-        
 
         $.ajax({
             type: "POST",
@@ -125,24 +52,123 @@ function updateSelect(options) {
 }
 
 function updatePontos(pontos){
-    var pontosArray = pontos.split(',');
+    let pontosArray = pontos.split(',');
 
-    $("#ponto1").val(pontosArray[0] || '');
-    $("#ponto2").val(pontosArray[1] || '');
-    $("#ponto3").val(pontosArray[2] || '');
-    $("#ponto4").val(pontosArray[3] || '');
-    $("#ponto5").val(pontosArray[4] || '');
-    $("#ponto6").val(pontosArray[5] || '');
+    let inputWrappers = document.querySelectorAll('.input-wrapper');
+    let errors = document.querySelectorAll('.erro');
+
+    inputWrappers.forEach(inputWrapper => {
+        inputWrapper.remove();
+    });
+
+    errors.forEach(erro => {
+        erro.remove()
+    })
+
+    pontosArray.forEach((ponto, index) => {
+        createInputElement(ponto, index);
+        
+    });
+}
+
+function updateErros() {
+    let elementsWrapper =[];
+    for (let i = 0; i < 10; i++) {
+        if (document.getElementById("erro-ponto" + i)) {
+
+            elementsWrapper.push({ input: "ponto" + i, error: document.getElementById("erro-ponto" + i) });
+        }
+    }
+
+    let pattern = /[^a-zA-Z0-9\s]/;
+    let textLength = 5;
+    elementsWrapper.forEach((el) => {
+        $('#' + el.input).keyup(function () {
+            let element = $(this);
+            let replaced;
+
+            if (pattern.test(element.val())) {
+                letterOnlyErrMsg(el.error)
+            } else if (element.val().length < textLength) {
+                textLengthErrMsg(el.error)
+            }
+
+            replaced = element.val().replace(pattern, '')
+            element.val(replaced)
+        })
+        $('#' + el.input).focusout(function () {
+            removeErrMsg(el.error)
+        })
+    });
+
+    const letterOnlyErrMsg = (erroID) => {
+        $(erroID).fadeIn('slow', function () {
+            $(this).html('O input aceita apenas letras.')
+        })
+    }
+
+    const textLengthErrMsg = (erroID) => {
+        $(erroID).fadeIn('slow', function () {
+            $(this).html('O input requer pelo menos 5 letras.')
+        })
+    }
+
+    const removeErrMsg = (erroID) => {
+        $(erroID).fadeOut(1000, function () {
+            $(this).html('')
+        })
+    }
 }
 
 $(document).ready(function () {
     $('#linhas').on('change', function () {
-        var pontos = $(this).find(':selected').data('pontos');
+        let pontos = $(this).find(':selected').data('pontos');
         updatePontos(pontos);
     });
 });
 
+function createInputElement(ponto, index) {
+    if(index === 0){
+        let numDeInputs = document.getElementsByClassName("input-wrapper").length;
+        if(numDeInputs > 10){
+            $(".add-button").hide();
+        } else {
+            $(".add-button").show();
+        }
+        index = numDeInputs;
+    }
+    
 
+    let form = document.getElementById('form');
+    let button = document.getElementById('add-button');
+
+    let inputWrapper = document.createElement('div');
+    inputWrapper.classList.add('input-wrapper');
+
+    let label = document.createElement('label');
+    label.setAttribute('for', 'ponto' + (index + 1));
+    label.classList.add('label');
+    label.textContent = 'Ponto ' + (index + 1) + ':';
+
+    let input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'ponto' + (index + 1));
+    input.classList.add('input');
+    input.setAttribute('required', 'true');
+    input.setAttribute('minlength', '5');
+    input.value = ponto;
+
+    let erro = document.createElement('div');
+    erro.setAttribute('id', 'erro-ponto' + (index + 1));
+    erro.classList.add('erro');
+
+    inputWrapper.appendChild(label);
+    inputWrapper.appendChild(input);
+
+    form.insertBefore(inputWrapper, button);
+    form.insertBefore(erro, button);
+    updateErros();
+}
 
 var abrirSubmenu = 1;
 function acaoSubmenu() {
